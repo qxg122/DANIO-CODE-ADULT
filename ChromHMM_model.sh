f675bb39fbb45798fb5bcac20371e244fa78f105
qxg122 <101339736+qxg122@users.noreply.github.com>
2024-07-12 19:09:23 +0100

    ChromHMM_model.sh

--- ChromHMM_model.sh
+++ ChromHMM_model.sh
@@ -3,22 +3,25 @@
 #SBATCH -c 8
 #SBATCH -o slurm-%J.out
 #SBATCH --time=09:00:00
-# Please state the number of array iteration at sbatch submission
 
 module purge; module load bluebear
 module load Java/11.0.2
 
-# careful, this is hardcoded, depends on the order of stage input!!!!
-
-#num_states=( 6 3 5 3 3 6 6 )
 state_num=10
 echo $state_num
 
-cd /rds/projects/m/muellerf-cage/Qianhong/ChromHMM_file/chromHMM_combined/
+while IFS= read -r line; do
+  arr+=("$line")
+done < ./Qianhong/tissue.txt
+
+acc=${arr[$SLURM_ARRAY_TASK_ID]}
 
 set -x
-java -mx10G -jar /rds/projects/m/muellerf-cage/Qianhong/ChromHMM/ChromHMM.jar BinarizeBam /rds/projects/m/muellerf-cage/Qianhong/ChromHMM/CHROMSIZES/danRer11.txt . combined_cell_mark_filetable.txt binary_bam_combined_out_10
 
-java -mx10G -jar /rds/projects/m/muellerf-cage/Qianhong/ChromHMM/ChromHMM.jar LearnModel -p 10 -printposterior -printstatebyline binary_bam_combined_out_10 ChromHMM__combinedout_10 $state_num danRer11
+cd ./Qianhong/ChromHMM_file/${acc}
+
+java -mx10G -jar ./Qianhong/ChromHMM/ChromHMM.jar BinarizeBam ./Qianhong/ChromHMM/CHROMSIZES/danRer11.txt . cell_mark_filetable.txt binary_bam_out
+
+java -mx10G -jar ./Qianhong/ChromHMM/ChromHMM.jar LearnModel -p 10 -printposterior -printstatebyline binary_bam_out ChromHMM_out_10 $state_num danRer11
 
 set +x
