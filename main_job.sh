90742e1c74fec32d663d02f350089a440abf7268
qxg122 <101339736+qxg122@users.noreply.github.com>
2024-07-12 17:16:01 +0100

    main_job.sh

--- main_job.sh
+++ main_job.sh
@@ -8,10 +8,10 @@ module load SRA-Toolkit/2.10.9-gompi-2020b
 
 while IFS= read -r line; do
   arr+=("$line")
-done < ./Qianhong/SRR_Acc_List_1.txt
+done < ./Qianhong/SRR_Acc_List.txt
 
 acc=${arr[$SLURM_ARRAY_TASK_ID]}
 
 cd ./Qianhong/fastq_k9
 
-fasterq-dump ${acc} -t ./Qianhong/fastq_k9
+fasterq-dump ${acc} -t ./Qianhong/fastq
