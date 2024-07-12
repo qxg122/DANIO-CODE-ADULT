372fe6b949519805d4662eedd9f0d37971b69d0d
qxg122 <101339736+qxg122@users.noreply.github.com>
2024-07-12 17:28:58 +0100

    Encode_ATAC_pipeline.sh

--- Encode_ATAC_pipeline.sh
+++ Encode_ATAC_pipeline.sh
@@ -2,15 +2,15 @@
 #SBATCH -J atacpipeline
 #SBATCH --array 0-12
 
-cd /rds/projects/m/muellerf-cage/Qianhong/
+cd ./Qianhong/
 
 module purge; module load bluebear
 module load Java/11.0.2
 
 while IFS= read -r line; do
   arr+=("$line")
-done < /rds/projects/m/muellerf-cage/Qianhong/atac_sampleList.txt
+done < ./Qianhong/atac_sampleList.txt
 
 atac=${arr[$SLURM_ARRAY_TASK_ID]}
 
-caper hpc submit /rds/homes/q/qxg122/pipelines/atac-seq-pipeline/atac.wdl -i /rds/projects/m/muellerf-cage/Qianhong/atac_input/${atac} --singularity --leader-job-name r
+caper hpc submit ./atac-seq-pipeline/atac.wdl -i ./Qianhong/atac_input/${atac} --singularity --leader-job-name r
