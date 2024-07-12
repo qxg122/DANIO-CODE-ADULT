d2e79ac4c9001f3c6253dd9f871bbe6a659034c5
qxg122 <101339736+qxg122@users.noreply.github.com>
2024-07-12 17:33:36 +0100

    Encode_ChIP_pipeline.sh

--- Encode_ChIP_pipeline.sh
+++ Encode_ChIP_pipeline.sh
@@ -2,15 +2,15 @@
 #SBATCH -J chipipeline
 #SBATCH --array 0-3
 
-cd /rds/projects/m/muellerf-cage/Qianhong/
+cd ./Qianhong/
 
 module purge; module load bluebear
 module load Java/11.0.2
 
 while IFS= read -r line; do
   arr+=("$line")
-done < /rds/projects/m/muellerf-cage/Qianhong/chip_sampleList_k9_me3.txt
+done < ./Qianhong/chip_sampleList.txt
 
 chip=${arr[$SLURM_ARRAY_TASK_ID]}
 
-caper hpc submit /rds/homes/q/qxg122/pipelines/chip-seq-pipeline2/chip.wdl -i /rds/projects/m/muellerf-cage/Qianhong/chip_input_k9/${chip} --singularity --leader-job-name v
+caper hpc submit ./chip-seq-pipeline2/chip.wdl -i ./Qianhong/chip_input/${chip} --singularity --leader-job-name v
